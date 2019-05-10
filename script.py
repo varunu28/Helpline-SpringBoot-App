a2f3c6b349b89d74adb11f3d14dc8fbfe379b696
varunu28 <varun.u28@gmail.com>
2019-05-10 08:16:45 -0700

    Updated script for random cases

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import requests
 import random
 import time
-import sys
+import queue
 
-API = "http://ec2-3-14-72-137.us-east-2.compute.amazonaws.com:8080/api/v1/entry"
+API = "http://ec2-18-217-63-232.us-east-2.compute.amazonaws.com:8080/api/v1/entry"
 
 locations = ["San Jose", "Paradise", "San Juan", "Alameda", "Alpine", "Contra Costa", "Del Norte", "Fresno", "Los Angeles", "Napa", "Nevada", 
 			"Sacramento", "San Francisco", "San Mateo", "Santa Clara", "Santa Cruz", "San Benito", "Santa Barbara", "San Bernardino", "San Diego", "Inyo",
@@ -38,29 +38,36 @@ contents = {
 sources = ["twitter", "facebook", "helpline"]
 counter = {}
 
-count = 2
+cases_sent = queue.Queue(maxsize=10)
 
-for i in range(count):
+while True:
+
+	if cases_sent.full():
+		case = cases_sent.get()
+		data = {
+			'location': case.split('|')[1],
+			'area': case.split('|')[0],
+	        'source': case.split('|')[2],
+	        'isrescued': 'true'
+		}
+		data['content'] = 'Rescued from ' + case.split('|')[0]
+		print(str(data))
+		r = requests.post(url = API, json = data)
+		print(str(r.status_code) + "\n")
+
 	data = {
 		'location': locations[random.randint(0, len(locations) - 1)],
 		'area': areas[random.randint(0, 3)],
-        'source': sources[random.randint(0, 2)]
+        'source': sources[random.randint(0, 2)],
+        'isrescued': 'false'
 	}
 	data['content'] = contents[data['area']][random.randint(0, len(contents[data['area']]) - 1)]
-
-	key = data['area'] + "|" + data['location']
-	if key not in counter.keys():
-		counter[key] = 1
-		data['isrescued'] = "false"
-	else:
-		if counter[key] % 5 == 0:
-			data['isrescued'] = "true"
-		else:
-			data['isrescued'] = "false"
-		counter[key] = counter[key] + 1
-
-	print (str(data))
+	key = data['area'] + "|" + data['location'] + "|" + data['source']
+	
+	cases_sent.put(key)
 
 	r = requests.post(url = API, json = data)
 	print(str(r.status_code) + "\n")
+	print (str(data))
+
 	time.sleep(5)
